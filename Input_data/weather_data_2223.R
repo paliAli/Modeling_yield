f5d961c6305c5cccd17c8fab66f7143663227782
paliAli <pavlackovaa@gmail.com>
2025-05-13 10:24:29 +0200

    Update weather

--- Input_data/weather_data_2223.R
+++ Input_data/weather_data_2223.R
@@ -14,7 +14,6 @@
 # Load the required libraries ----
 install.packages("nasapower")
 library(nasapower)
-library(ggplot2)
 library(dplyr)
 library(lubridate)
 
@@ -24,13 +23,14 @@ latitudes <- seq(35, 72, by = 1) # Just to check - need to change later
 longitudes <- seq(-30, 50, by = 1) 
 
 # Define a sowing date and a harvest date
-startDate <- "2023-01-01"
+startDate <- "2024-01-01"
 endDate <- "2025-01-01"
 dates <- c(startDate, endDate)
 
 # Download the data from NASA POWER ----
-Variables <- c("ALLSKY_SFC_SW_DWN", "PRECTOTCORR", "RH2M", "T2MDEW", "T2M_MAX", "T2M_MIN", "WS2M", "VAP")
-VarName <- c("Solar", "ppt", "RH", "Tdew", "Tmax", "Tmin", "windspeed", "vap")
+Variables <- c("ALLSKY_SFC_SW_DWN", "PRECTOTCORR", "RH2M", "T2MDEW", "T2M_MAX", "T2M_MIN", "WS2M")
+VarName <- c("Solar", "ppt", "RH", "Tdew", "Tmax", "Tmin", "windspeed")
+
 
 # Download the data from NASA POWER
 Europe_weather_data <- data.frame()
@@ -41,23 +41,30 @@ for (lat in latitudes) {
       community = "ag",
       lonlat = c(lon, lat),
       pars = Variables,
-      dates =dates, 
+      dates = dates, 
       temporal_api = "daily"
     )
+    
+    # Rename columns using VarName
+    names(data)[names(data) %in% Variables] <- VarName
+    
     Europe_weather_data <- rbind(Europe_weather_data, data)
   }
 }
 
-# Add doy and calculate average temperature 
-Europe_weather_data <- weather %>%
+# Edit the data frame ----
+summary(Europe_weather_data)
+class(Europe_weather_data$YYYYMMDD)
+# Rename YYYYMMDD to date
+colnames(Europe_weather_data)[colnames(Europe_weather_data) == "YYYYMMDD"] <- "Date"
+
+# Calculate average temperature 
+Europe_weather_data <- Europe_weather_data %>%
   mutate(
-    DOY = yday(date),
     Tmean = (Tmax + Tmin) / 2
   )
 
 #save the data
 write.csv(Europe_weather_data, "Europe_weather_data.csv", row.names = FALSE)
-#read the data
-Europe_weather_data <- read.csv("Europe_weather_data.csv")
-#convert the date to Date type
-Europe_weather_data$Date <- as.Date(Europe_weather_data$Date)
+
+
