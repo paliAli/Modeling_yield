3ee7a4013c322c1548d8adbb7efce611eeb29826
paliAli <pavlackovaa@gmail.com>
2025-05-13 08:04:38 +0200

    Input data calculations
    Created a new script to provide weather data and added information to the DVS calculation

---
+++ Input_data/weather_data.R
@@ -0,0 +1,16 @@
+##%######################################################%##
+#                                                          #
+####                  Provide Weather                   ####
+#                                                          #
+##%######################################################%##
+
+-----------------------------------------------------------
+  # Written by Alena Pavlackova
+  # ETH Biogeochemical modeling group project
+  
+  # The script provides daily weather data
+-----------------------------------------------------------
+
+# Required libraries ----
+
+# Prepare the data ----
--- Scripts/DVS_calculation.R
+++ Scripts/DVS_calculation.R
@@ -4,11 +4,27 @@
 #                                                          #
 ##%######################################################%##
 
-?approx
+-----------------------------------------------------------
+  # Written by Alena Pavlackova
+  # ETH Biogeochemical modeling group project
+  
+  # The script calculates the total temperature sum
+  # and changes the DVS accordingly
+-----------------------------------------------------------
+
+# Required libraries -----
+
+
+# Prepare the data ----
+# Set working directory 
+setwd("~/GitHub/Modeling_yield")
 # Load in crop data
-crop <- source("~/GitHub/Modeling_yield/Input_data/crop_data.R")
+crop <- source("Input_data/crop_data.R")
+# Load in weather dara
+weather <- 
 
 # Define crop TSUM thresholds ----
 TSUM_stages <- c(crop$value[["TSUMEM"]],crop$value[["TSUMEM"]] + crop$value[["TSUM1"]], crop$value[["TSUMEM"]] + crop$value[["TSUM1"]] + crop$value[["TSUM2"]]) # Cumulative TSUM values
 DSV_stages <- c(0, 1, 2) # Corresponding development stages
 
+?approx
