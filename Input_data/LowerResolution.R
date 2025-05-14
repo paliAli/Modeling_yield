9872b9aa65fcd1f18ab730e0008b5b0890ba25aa
Pasc124 <pascal.aeschimann@gmx.ch>
2025-05-14 21:55:45 +0200

    Add files via upload
    Coordinates for Weather Data

---
+++ Input_data/LowerResolution.R
@@ -0,0 +1,89 @@
+# This script resamples the map file to a lower resolution to make it feasible for the analysis
+# The original map file is 100 x 100m, the resampled file is 1 x 1 km
+setwd("~/Macbook/Studium/Master/Biogeochem. Modeling")
+
+
+install.packages(c("viridisLite", "ggpubr"), type= "binary")
+install.packages("maptools", type= "binary")
+
+library(readxl)
+library(ggplot2)
+library(ggthemes)
+library(viridis)
+library(maptools)
+library(ggpubr)
+library(raster)
+# library(rgdal)
+# library(sp)
+
+
+
+#original pixel size 100 x 100m
+
+map=raster("aggregated_landcover_raster_1x1km.tif")
+res(map)
+crs(map)
+
+plot(map)
+
+
+# Define the aggregation factor (from 100m to 1km)
+agg_factor <- 50
+
+# Aggregate the raster using the modal function
+aggregated_raster <- aggregate(map, fact = agg_factor, fun = modal)
+
+# Save the aggregated raster
+writeRaster(aggregated_raster, "aggregated_landcover_raster_50x50km.tif", format = "GTiff")
+
+# Plot the aggregated raster
+plot(aggregated_raster)
+# Check the resolution of the resampled raster
+res(aggregated_raster)
+crs(aggregated_raster)
+
+
+# Create a data frame with the aggregated raster values
+df <- as.data.frame(aggregated_raster, xy = TRUE)
+str(df)
+
+
+unique(df$aggregated_landcover_raster_1x1km)
+
+
+# FILE: How use ESRI CLC raster files in QGIS.doc shows which LU corresponds to grasslands
+# 12 = arable land
+
+Wanted_Points<-df[df[,3] %in% c(12),]
+
+plot(Wanted_Points$y~Wanted_Points$x, pch=20, col="red", xlab="X", ylab="Y", main="Wanted Points")
+
+
+str(Wanted_Points)
+
+
+
+
+# Rename the land use column
+colnames(Wanted_Points)[3]<- "Land_Use"
+
+# Replace 18 by Pastures and 26 by Natural grasslands
+Wanted_Points$Land_Use[Wanted_Points$Land_Use==18]<- "Pastures"
+Wanted_Points$Land_Use[Wanted_Points$Land_Use==26]<- "Natural grasslands"
+Wanted_Points$Land_Use[Wanted_Points$Land_Use==22]<- "Agroforestry areas"
+
+# Store the data frame in a CSV file
+write.table(Wanted_Points, "../../Wanted_Grassland_Points10km.csv", row.names = FALSE,sep=";")
+
+
+
+
+
+
+
+
+
+
+
+
+
